0f9b90036e222ff99fac610c6b36a2972a742bbe
Annalise Murphy <nali247@gmail.com>
2020-12-14 20:20:00 -0800

    taking a break - to be continued

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -5,18 +5,33 @@ import originalZen from './images/originalZen.png';
 export default function Loading() {
   return (
     <>
-     <div className="loading-container">
-       <div className="loading-screen"></div>
+      <div className="loading-container">
+
+        <div className="loading-screen"></div>
        
-       <div className="loader">
-         <div className="zenOne ring"></div>
+        <div className="loader">
+          <div className="zenOne ring"></div>
           <img src={originalZen} alt="" />
-       </div>
+        </div>
 
-       <div className="zenTwo ring">
-         <img src={originalZen} alt="" />
-       </div>
+        <div className="zenTwo ring">
+          <img src={originalZen} alt="" />
+        </div>
+        {/* think about replacing logo with NameBranding */}
+      <div className="logo">
+        <ion-icon name="git-compare"></ion-icon>
+          FilesFiddle
+      </div>
+      
+      <div className="contact">GET IN TOUCH</div>
 
+      <div className="menu">
+        <ion-icon name="options"></ion-icon>
+      </div>
+
+      <div className="header">
+        {/* PICK UP FROM HEREEEEEEE */}
+      </div>
      </div>
     
     </>
