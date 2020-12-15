5e001b7ff6e0b10166d53d5bb242d2aae37631e0
Annalise Murphy <nali247@gmail.com>
2020-12-14 22:08:24 -0800

    Loading ready for styling

--- src/Loading.jsx
+++ src/Loading.jsx
@@ -30,9 +30,44 @@ export default function Loading() {
       </div>
 
       <div className="header">
-        {/* PICK UP FROM HEREEEEEEE */}
+
+        {/* NOTE: Change h1 classNaming convention */}
+        <h1 className="ml7" id="title">
+          <span className="text-wrapper">
+            <span className="letters">Annalise Murphy</span>
+          </span>
+        </h1>
+
+          <p id="tagline" className="p2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam dignissimos tenetur similique magni adipisci perferendis totam tempore laborum sint beatae modi necessitatibus cumque debitis aut, velit odio dolorum, dolorem aspernatur?
+          </p>
+
+          <br />
+
+          <p id="tagline" className="p2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus animi corporis magnam illo aspernatur nesciunt rerum officiis enim neque unde, maiores fuga asperiores dolorem totam ea sunt quia consequuntur itaque.
+          </p>
+
+          <div className="buttons">
+            <button id="one">LEARN MORE</button>
+            <button id="two">BUY NOW</button>
+          </div>
       </div>
-     </div>
+
+      <div className="bottom-text">+0.0001db</div>
+
+      <div className="copyright">2020 by Annalise Murphy. All rights reserved.</div>
+
+      <div className="media">
+        <ul>
+          <li><ion-icon className="logo-facebook"></ion-icon></li>
+          <li><ion-icon className="logo-instagram"></ion-icon></li>
+          <li><ion-icon className="logo-twitter"></ion-icon></li>
+          <li><ion-icon className="logo-youtube"></ion-icon></li>
+        </ul>
+      </div>
+
+      </div>
     
     </>
   )
