6c41f32b2f3f54bc8c94e87a2ab410e420f52435
Annalise Murphy <nali247@gmail.com>
2020-12-21 19:18:49 -0800

    still working script.js

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
-import { TweenMax, Expo } from 'gsap/gsap-core';
+import { useEffect } from 'react';
+import { TweenMax, Expo, Power3, TimelineMax } from 'gsap/gsap-core';
+
 
 const Loader = () => {
 
@@ -9,20 +10,100 @@ const Loader = () => {
       top: "-110%",
       ease: Expo.easeInOut
     });
+
     TweenMax.from('.logo', 3, {
       delay: 8.4,
       opacity: 0,
       y: 20,
       ease: Expo.easeInOut
     });
+
+    TweenMax.from('.options', 3, {
+      delay: 8.4,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from('.bottom-text', 3, {
+      delay: 8.7,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from('.copyright', 3, {
+      delay: 8.9,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.staggerFrom('.media ul li', 2, {
+      delay: 8.7,
+      opacity: 0,
+      y: 20,
+      ease: Power3.easeInOut
+    }, 0.1);
+
+    TweenMax.from('.menu', 3, {
+      delay: 8.8,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from('.p1', 3, {
+      delay: 9,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from('.p2', 3, {
+      delay: 9.2,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
+
+    TweenMax.from('#one', 3, {
+      delay: 9.4,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
   
+    TweenMax.from('#two', 3, {
+      delay: 9.7,
+      opacity: 0,
+      y: 20,
+      ease: Expo.easeInOut
+    });
+
+    const t1 = new TimelineMax();
+
+    t1.from('.ringOne', 4, {
+      delay: 0.4,
+      opacity: 0,
+      y: 40,
+      ease: Expo.easeInOut
+    }, '-=5').to('.ringOne', 4, {
+      delay: 0.4,
+      x: 40,
+      ease: Expo.easeInOut
+    }).to('.ringTwo', 4,{
+      delay: 0.9,
+      x: 40,
+      ease: Expo.easeInOut
+    }, '-=5');
+
+    const textWrapper = document.querySelector('.ml7 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, '<span className={letter}>$&</span>');
+
+
   }, []);
 
-  return (
-    <>
-    
-    </>
-  );
 };
 
 export default Loader;
