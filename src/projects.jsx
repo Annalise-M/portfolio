41ba54be939d3bc3252d3ee8e7902806bdb87560
Annalise-M <nali247@gmail.com>
2021-02-19 00:00:09 -0800

    got styles imported to Parallax- prepped for next step

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,36 +1,29 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 import { Parallax, ParallaxLayer } from 'react-spring/renderprops-addons';
+import { styles } from './App.scss';
 
 export default function Projects() {
     let parallax;
     return (
-        <Parallax pages={2} ref={ref => parallax = ref}>
-            <ParallaxLayer offset={0.3} speed={0}>
-                <span>Scroll down bitch!</span>
-            </ParallaxLayer>
-            <ParallaxLayer offset={1.2} speed={0}>
-                <span>Scroll down further bitch!</span>
-            </ParallaxLayer>
-            <ParallaxLayer offset={0.9} speed={3}>
-                <span>Fast one bitch!</span>
-            </ParallaxLayer>
-            <ParallaxLayer offset={-0.2} speed={1.3}>
-                <span>Go the other direction bitch!</span>
-            </ParallaxLayer>
-        </Parallax>
+        <div>
+            <h1>
+                <section styles={styles}>
+                    <Parallax pages={2} ref={ref => parallax = ref}>
+                        <ParallaxLayer offset={0.3} speed={0}>
+                            <span>Scroll down bitch!</span>
+                        </ParallaxLayer>
+                        <ParallaxLayer offset={1.2} speed={0}>
+                            <span>Scroll down further bitch!</span>
+                        </ParallaxLayer>
+                        <ParallaxLayer offset={0.9} speed={3}>
+                            <span>Fast one bitch!</span>
+                        </ParallaxLayer>
+                        <ParallaxLayer offset={-0.2} speed={1.3}>
+                            <span>Go the other direction bitch!</span>
+                        </ParallaxLayer>
+                    </Parallax>
+                </ section>
+            </h1>
+        </div>
     );
 }
-
-
-// export default function Projects() {
-//         return (
-//             <Parallax pages={3} scrolling={false} horizontal ref={ref => (this.parallax = ref)}>
-//                 <ParallaxLayer offset={0} speed={0.5}>
-//                     <span onClick={() => this.parallax.scrollTo(1)}>Scroll down here!</span>
-//                 </ParallaxLayer>
-//             </Parallax>
-//         )
-// }
-
-// ReactDOM.render(<Projects />, document.getElementById('root'));
